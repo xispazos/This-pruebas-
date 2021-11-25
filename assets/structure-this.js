9d8b0160b9cf62f414ea64a1ee0f679f1e58c87d
xispazos <tcarrero81@gmail.com>
2021-11-25 18:54:05 +0100

    estructura this

---
+++ assets/structure-this.js
@@ -0,0 +1,16 @@
+function usuario (nombre,apellido,edad,genero) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+    this.genero = genero;
+
+}
+
+//USUARIO 1
+
+let usuario1 = new usuario ("Laura", "Carrero",30, "femenino");
+
+//USUARIO 2
+
+let usuario2 = new usuario ("Lucia", "Sol", 40, "femenino");
+
